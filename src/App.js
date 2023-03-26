57c7ae8dbc7f6dd69aad36164a9e7f26fa5d3ae7
Astha <asthakarn216@gmail.com>
2023-03-26 13:57:10 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-2xl bg-[#31de93] w-1/2 h-[20rem] shadow-2xl flex gap-5">
+      <div>
+        jwelll
+      </div>
+      <div>
+        earing
+      </div>
+    </div>
+  );
+}
+
+export default App;
