68b474f86fc63562e593c353a08aad714de37d96
Astha <asthakarn216@gmail.com>
2023-03-29 13:09:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 
 import './App.css';
+import './index.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './Routes/Home';
+import About from './Routes/About';
+import Contact from './Routes/Contact';
+import Products from './Routes/Products';
+import ErrorPage from './Routes/ErrorPage';
 
 function App() {
   return (
-    <div className="text-2xl bg-[#31de93] w-1/2 h-[20rem] shadow-2xl flex gap-5">
-      <div>
-        Hello World
-      </div>
-      <div>
-        THis is my website
-      </div>
-      <p>asthakarn ecommerce</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="*" element={<ErrorPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from "../assets/paperFlourish.png";
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+      <img src={logo}/>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
