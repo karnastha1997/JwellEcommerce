3f5c9bcf4987c5d187864ca599eaf30943e80d81
Astha <asthakarn216@gmail.com>
2023-03-29 13:27:10 +0530

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import logo from "../assets/paperFlourish.png";
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <>
       <div>
-      <img src={logo}/>
+        <Link to="/">
+          <img src={logo} />
+        </Link>
+
+        <ul >
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/project">Project</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/skills">Skills</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
       </div>
     </>
   )
